acc920c505cf379f3684d5f74f561c9768f680a2
xuejipeng <xuejipeng@sensetime.com>
2020-03-07 22:15:57 +0800

    add docs dir

---
+++ docs/cronjob-tutorial/testdata/emptymain-cn.go
@@ -0,0 +1,119 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+// +kubebuilder:docs-gen:collapse=Apache License
+
+/*
+
+我们的 main.go 文件最开始会 import 一些基础的 package。 例如：
+
+- 比较核心的库： [controller-runtime](https://godoc.org/sigs.k8s.io/controller-runtime)
+- controller-runtime 的默认日志库：Zap（稍后会详细介绍）
+
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	// +kubebuilder:scaffold:imports
+)
+
+/*
+
+每组 controller 都需要一个 [*Scheme*](https://book.kubebuilder.io/cronjob-tutorial/gvks.html#err-but-whats-that-scheme-thing)，
+Scheme 会提供 Kinds 与 Go types 之间的映射关系(现在你只需要记住这一点)。
+在编写 API 定义时，我们将会进一步讨论 Kinds。
+
+*/
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+
+	// +kubebuilder:scaffold:scheme
+}
+
+/*
+此时，我们 main.go 的功能相对来说比较简单：
+
+1. 为 metrics 绑定一些基本的 flags。
+
+2. 实例化一个 manager，用于跟踪我们运行的所有 controllers，并设置 shared caches 和可以连接到 API server 的 k8s clients 实例，并将 Scheme 配置传入 manager。
+
+3. 运行我们的 manager, 而 manager 又运行所有的 controllers 和 webhook。manager 会一直处于运行状态，直到收到正常关闭信号为止。
+这样，当我们的 operator 运行在 Kubernetes 上时，我们可以通过优雅的方式终止这个 Pod。
+
+虽然目前我们还没有什么可以运行，但是请记住 `+kubebuilder:scaffold:builder` 注释的位置 -- 很快那里就会变得有趣。
+*/
+
+func main() {
+	var metricsAddr string
+	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
+	flag.Parse()
+
+	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{Scheme: scheme, MetricsBindAddress: metricsAddr})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	/*
+	   请注意：下面代码如果指定了 Namespace 字段, controllers 仍然可以 watch cluster 级别的资源(例如Node), 但是对于 namespace 级别的资源，cache 将仅可以缓存指定 namespace 中的资源。
+	*/
+	mgr, err = ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:             scheme,
+		Namespace:          namespace,
+		MetricsBindAddress: metricsAddr,
+	})
+
+	/*
+	   上面的示例将 operator 应用的获取资源范围限制在了单个 namespace。在这种情况下，建议将默认的 ClusterRole 和 ClusterRoleBinding 分别替换​​为 Role 和 RoleBinding 来将授权限制于此名称空间。
+	   有关更多信息，请参见如何使用 [RBAC Authorization](https://kubernetes.io/docs/reference/access-authn-authz/rbac/)。
+
+	   除此之外，你也可以使用 MultiNamespacedCacheBuilder 来监视一组特定的 namespaces：
+	*/
+
+	var namespaces []string // List of Namespaces
+
+	mgr, err = ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:             scheme,
+		NewCache:           cache.MultiNamespacedCacheBuilder(namespaces),
+		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
+	})
+
+	/*
+		有关更多信息，请参见 [MultiNamespacedCacheBuilder](https://godoc.org/github.com/kubernetes-sigs/controller-runtime/pkg/cache#MultiNamespacedCacheBuilder)
+	*/
+
+	// +kubebuilder:scaffold:builder
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}
---
+++ docs/cronjob-tutorial/testdata/project/api/v1/cronjob_types.go
@@ -0,0 +1,180 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+// +kubebuilder:docs-gen:collapse=Apache License
+
+package v1
+
+import (
+	batchv1beta1 "k8s.io/api/batch/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// +kubebuilder:docs-gen:collapse=Imports
+
+/*
+首先，让我们看一下 `spec`，如我们之前说的，`spec` 表明**期望的状态**，所以有关 controller 的任何 "inputs" 都在这里声明。
+
+一个基本的 CronJob 需要以下功能：
+
+- 一个 schedule(调度器) -- (CronJob 中的 "Cron")
+- 用来运行 Job 的模板 -- (CronJob 中的 "Job")
+
+为了让用户体现更好，我们也需要一些额外的功能：
+
+- 一个截止时间（`StartingDeadlineSeconds`）, 如果错过了这个截止时间，Job 将会等到下一个调度时间点再被调度。
+- 如果多个 Job 同时启动要怎么做（`ConcurrencyPolicy`）（等待？停掉最老的一个？还是同时运行？）
+- 一个暂停(`Suspend`)功能，以防止 Job 在运行过程中出现什么错误。
+- 限制历史 Job 的数量（`SuccessfulJobsHistoryLimit`）
+
+请记住，因为 job 不会读取自己的状态，所以我们需要用一些方式去跟踪一个 job 是否运行，
+我们可以用至少一个 old job 来完成这个功能。（不明白，回头再改）
+
+我们会用几个标记 (`// +comment`) 去定义一些额外的数据，这些标记在
+[controller-tools](https://github.com/kubernetes-sigs/controller-tools)
+生成 CRD manifest 时会被使用。
+稍后我们还将看到，controller-tools 还会使用 GoDoc 来为字段生成描述信息。
+*/
+
+// CronJobSpec defines the desired state of CronJob
+type CronJobSpec struct {
+	// +kubebuilder:validation:MinLength=0
+
+	// The schedule in Cron format, see https://en.wikipedia.org/wiki/Cron.
+	Schedule string `json:"schedule"`
+
+	// +kubebuilder:validation:Minimum=0
+
+	// Optional deadline in seconds for starting the job if it misses scheduled
+	// time for any reason.  Missed jobs executions will be counted as failed ones.
+	// +optional
+	StartingDeadlineSeconds *int64 `json:"startingDeadlineSeconds,omitempty"`
+
+	// Specifies how to treat concurrent executions of a Job.
+	// Valid values are:
+	// - "Allow" (default): allows CronJobs to run concurrently;
+	// - "Forbid": forbids concurrent runs, skipping next run if previous run hasn't finished yet;
+	// - "Replace": cancels currently running job and replaces it with a new one
+	// +optional
+	ConcurrencyPolicy ConcurrencyPolicy `json:"concurrencyPolicy,omitempty"`
+
+	// This flag tells the controller to suspend subsequent executions, it does
+	// not apply to already started executions.  Defaults to false.
+	// +optional
+	Suspend *bool `json:"suspend,omitempty"`
+
+	// Specifies the job that will be created when executing a CronJob.
+	JobTemplate batchv1beta1.JobTemplateSpec `json:"jobTemplate"`
+
+	// +kubebuilder:validation:Minimum=0
+
+	// The number of successful finished jobs to retain.
+	// This is a pointer to distinguish between explicit zero and not specified.
+	// +optional
+	SuccessfulJobsHistoryLimit *int32 `json:"successfulJobsHistoryLimit,omitempty"`
+
+	// +kubebuilder:validation:Minimum=0
+
+	// The number of failed finished jobs to retain.
+	// This is a pointer to distinguish between explicit zero and not specified.
+	// +optional
+	FailedJobsHistoryLimit *int32 `json:"failedJobsHistoryLimit,omitempty"`
+}
+
+/*
+我们自定义了一个类型（`ConcurrencyPolicy`）来保存我们的并发策略。
+它实际上只是一个字符串，但是这个类型名称提供了额外的说明，
+我们还将验证附加到类型上,而不是字段上，从而使验证更易于重用。
+*/
+
+// ConcurrencyPolicy describes how the job will be handled.
+// Only one of the following concurrent policies may be specified.
+// If none of the following policies is specified, the default one
+// is AllowConcurrent.
+// +kubebuilder:validation:Enum=Allow;Forbid;Replace
+type ConcurrencyPolicy string
+
+const (
+	// AllowConcurrent allows CronJobs to run concurrently.
+	AllowConcurrent ConcurrencyPolicy = "Allow"
+
+	// ForbidConcurrent forbids concurrent runs, skipping next run if previous
+	// hasn't finished yet.
+	ForbidConcurrent ConcurrencyPolicy = "Forbid"
+
+	// ReplaceConcurrent cancels currently running job and replaces it with a new one.
+	ReplaceConcurrent ConcurrencyPolicy = "Replace"
+)
+
+/*
+下面，让我们设计 `status`,它用来存储观察到的状态。它包含我们希望用户或其他 controllers 能够获取的所有信息
+
+我们将保留一份正在运行的 Job 列表，以及上一次成功运行 Job 的列表。
+注意，如上所述，我们使用 metav1.Time 而不是 time.Time 来获得稳定的序列化。
+*/
+
+// CronJobStatus defines the observed state of CronJob
+type CronJobStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// A list of pointers to currently running jobs.
+	// +optional
+	Active []corev1.ObjectReference `json:"active,omitempty"`
+
+	// Information when was the last time the job was successfully scheduled.
+	// +optional
+	LastScheduleTime *metav1.Time `json:"lastScheduleTime,omitempty"`
+}
+
+/*
+<span id="installation"></span>
+最后我们只剩下了 `CronJob` 结构体，如之前说的，我们不需要修改改结构体的任何内容，
+但是我们希望操作改 Kind 像操作 Kubernetes 内置资源一样，所以我们需要增加一个 mark `+kubebuilder:subresource:status`,
+来声明一个status subresource, 关于 subresource 的更多信息可以参考
+[k8s 文档](https://kubernetes.io/docs/tasks/access-kubernetes-api/custom-resources/custom-resource-definitions/#subresources)
+*/
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// CronJob is the Schema for the cronjobs API
+type CronJob struct {
+	/*
+	 */
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CronJobSpec   `json:"spec,omitempty"`
+	Status CronJobStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// CronJobList contains a list of CronJob
+type CronJobList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CronJob `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&CronJob{}, &CronJobList{})
+}
+
+// +kubebuilder:docs-gen:collapse=Root Object Definitions
