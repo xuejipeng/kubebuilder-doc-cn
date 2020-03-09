1ee25b7960f1822445d99f2aea5c95ef00293ba0
xuejipeng <xuejipeng@sensetime.com>
2020-03-09 22:50:20 +0800

    fix error and add note

--- docs/cronjob-tutorial/testdata/project/api/v1/cronjob_types.go
+++ docs/cronjob-tutorial/testdata/project/api/v1/cronjob_types.go
@@ -42,10 +42,10 @@ import (
 - 一个暂停(`Suspend`)功能，以防止 Job 在运行过程中出现什么错误。
 - 限制历史 Job 的数量（`SuccessfulJobsHistoryLimit`）
 
-请记住，因为 job 不会读取自己的状态，所以我们需要用一些方式去跟踪一个 job 是否运行，
-我们可以用至少一个 old job 来完成这个功能。（不明白，回头再改）
+请记住，因为 job 不会读取自己的状态，所以我们需要用一些方式去跟踪一个 CronJob 是否已经运行过 Job，
+我们可以用至少一个 old job 来完成这个功能。
 
-我们会用几个标记 (`// +comment`) 去定义一些额外的数据，这些标记在
+我们会用几个标记 (`//+comment`) 去定义一些额外的数据，这些标记在
 [controller-tools](https://github.com/kubernetes-sigs/controller-tools)
 生成 CRD manifest 时会被使用。
 稍后我们还将看到，controller-tools 还会使用 GoDoc 来为字段生成描述信息。
@@ -99,7 +99,8 @@ type CronJobSpec struct {
 /*
 我们自定义了一个类型（`ConcurrencyPolicy`）来保存我们的并发策略。
 它实际上只是一个字符串，但是这个类型名称提供了额外的说明，
-我们还将验证附加到类型上,而不是字段上，从而使验证更易于重用。
+我们还将验证附加到类型上，而不是字段上，从而使验证更易于重用。
+`// +kubebuilder:validation:Enum=Allow;Forbid;Replace` 表示这个 `ConcurrencyPolicy` 只接受 `Allow`、`Forbid`、`Replace` 这三个值。
 */
 
 // ConcurrencyPolicy describes how the job will be handled.
@@ -124,7 +125,7 @@ const (
 /*
 下面，让我们设计 `status`,它用来存储观察到的状态。它包含我们希望用户或其他 controllers 能够获取的所有信息
 
-我们将保留一份正在运行的 Job 列表，以及上一次成功运行 Job 的列表。
+我们将保留一份正在运行的 Job 列表，以及最后一次成功运行 Job 的时间。
 注意，如上所述，我们使用 metav1.Time 而不是 time.Time 来获得稳定的序列化。
 */
 
@@ -144,7 +145,7 @@ type CronJobStatus struct {
 
 /*
 <span id="installation"></span>
-最后我们只剩下了 `CronJob` 结构体，如之前说的，我们不需要修改改结构体的任何内容，
+最后我们只剩下了 `CronJob` 结构体，如之前说的，我们不需要修改该结构体的任何内容，
 但是我们希望操作改 Kind 像操作 Kubernetes 内置资源一样，所以我们需要增加一个 mark `+kubebuilder:subresource:status`,
 来声明一个status subresource, 关于 subresource 的更多信息可以参考
 [k8s 文档](https://kubernetes.io/docs/tasks/access-kubernetes-api/custom-resources/custom-resource-definitions/#subresources)
